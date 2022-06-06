43a1e907e4e17e9a040af419666826a1e6cf4d60
HP\HP <sontolmomentod@gmail.com>
2022-06-07 05:46:41 +0700

    F1E120018_MadilJabbarsah

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\kategori;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         $kategori = DB::table('kategoris')->get();
+         $data = [];
+         $label = [];
+
+         foreach ($kategori as $i => $v) {
+             $value[$i] = DB::table('produks')->where('id_kategori',$v->id)->count();
+             $label[$i] = $v->nama;
+         }
+        return view('home')
+             ->with('value',json_encode($value))
+             ->with('label',json_encode($label));
+    }
+}
